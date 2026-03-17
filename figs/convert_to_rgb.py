e83bce7c5b3710cb067052e3860cd1222eb88013
Haruya Ishida <harusaku.110906@gmail.com>
2026-03-17 12:06:11 +0900

    図に線が入る問題を修正 アルファチャンネルが原因でした

---
+++ figs/convert_to_rgb.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+
+base = os.path.dirname(os.path.abspath(__file__))
+files = [
+    'TOC.png', 'mapping_results.png', 'mapping_colormaps.png',
+    'HOPG_strong_force.png', 'schematics_of_measurement.png', 'scaling_result.png'
+]
+
+for fname in files:
+    path = os.path.join(base, fname)
+    img = Image.open(path).convert('RGBA')
+    bg = Image.new('RGB', img.size, (255, 255, 255))
+    bg.paste(img, mask=img.split()[3])  # アルファチャンネルをマスクとして白背景に合成
+    bg.save(path)
+    print(f'Done: {fname}')
+
+print('All done.')
